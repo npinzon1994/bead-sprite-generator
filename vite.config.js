c0866ec12b77362aa01336bcb60a9d35cd5b013e
Nikki Pinzon <npinzon1994@gmail.com>
2026-07-07 21:23:47 -0400

    updated base name to remote repository name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pixel-counter/",
+  base: "/bead-sprite-generator/",
   plugins: [react()],
   test: {
     globals: true,
